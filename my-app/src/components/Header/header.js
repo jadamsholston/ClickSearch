f88fbb66c8c7e77a7c14cdb89334e28319794b3c
Jordan <j.adamsholston@gmail.com>
2018-06-30 16:19:07 -0500

    components added to minipulate gamespace and styling

--- my-app/src/components/Header/header.js
+++ my-app/src/components/Header/header.js
@@ -4,7 +4,7 @@ import './header.css';
 const Header = props => (
     <div className="row header">
         <h2 className="col-sm text-center header-text">Clicky Game</h2>
-        <h3 className="col-sm text-center header-text"> Click on an image to earn points, but don't click on any more than once!</h3>
+        <h className="col-sm text-center header-text"> Click on an image to earn points, but don't click on any more than once!</h3>
         <h3 className='col-sm text-center header-text'>Score: {props.score} | Top Score: {props.topScore}</h3>
     </div>
 );
